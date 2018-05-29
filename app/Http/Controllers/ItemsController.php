7d9bae098826b9da63e8961c11f5b7ac920c5ca6
EC2 Default User <ec2-user@ip-172-31-42-43.us-east-2.compute.internal>
2018-05-29 12:43:22 +0000

    rakuten api

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Item;
+
+class ItemsController extends Controller
+{
+    // 楽天市場にある商品検索
+    public function create()
+    {
+        $keyword = request()->keyword; // フォームから送信される検索ワードを取得
+        $items = []; // view表示エラー回避のためitemsの初期化（空の配列）
+        if ($keyword) {
+            // 楽天APIを使用するための設定
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+            
+            // 楽天市場での商品検索
+            $rws_response = $client->execute('IchibaItemSearch', [
+                'keyword' => $keyword,
+                'imageFlag' => 1, // 1 : 商品画像ありの商品のみを検索対象とする
+                'hits' => 20, // 1ページあたりの取得件数
+                ]);
+                
+            // 扱いやすいように Item としてインスタンスを作成する（保存はしない）
+            foreach ($rws_response->getData()['Items'] as $rws_item) {
+                $item = new Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128×128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+        
+        return view('items.create', [
+            'keyword' => $keyword,
+            'items' => $items,
+            ]);
+    }
+}
---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'search')
+
+@section('content')
+    <div class="search">
+        <div class="row">
+            <div class="text-center">
+                {!! Form::open(['route' => 'items.create', 'method' => 'get', 'class' => 'form-inline']) !!}
+                    <div class="form-group">
+                        {!! Form::text('keyword', $keyword, ['class' => 'form-contol input-lg', 'placeholder' => 'キーワードを入力', 'size' => 40]) !!}
+                    </div>
+                    {!! Form::button('<span class="glyphicon glyphicon-search" aria-hidden="true"></span> 検索', ['class' => 'btn btn-success btn-lg', 'type' => 'submit']) !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+
+    @include('items.items', ['items' => $items])
+@endsection
